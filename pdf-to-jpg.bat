641bf718def379d109cb6bc104733157863eaba6
Kuba Szostak <kuba.szostak@gmail.com>
2017-09-08 13:40:10 +0200

    Update pdf-to-jpg.bat

--- pdf-to-jpg.bat
+++ pdf-to-jpg.bat
@@ -4,7 +4,7 @@ echo.
 
 set DPI=300
 set ALPHABITS=2
-set QUALITY=95
+set QUALITY=80
 set FIRSTPAGE=1
 set LASTPAGE=9999
 REM MEMORY in MB
@@ -16,8 +16,6 @@ set JPGFILE=%PDFFILE:.pdf=-%%%d.jpg
 set GS=%2
 IF "%GS%" == "" (
 	set GS="bin/gswin32c.exe"
-	echo Loaded in 32bit mode
-	echo.
 )
 
 %GS% -sDEVICE=jpeg -sOutputFile=%JPGFILE% -r%DPI% -dNOPAUSE -dFirstPage=%FIRSTPAGE% -dLastPage=%LASTPAGE% -dJPEGQ=%QUALITY% -dGraphicsAlphaBits=%ALPHABITS%  -dTextAlphaBits=%ALPHABITS%  -dNumRenderingThreads=4 -dBufferSpace=%MEMORY%000000  -dBandBufferSpace=%MEMORY%000000 -c %MEMORY%000000 setvmthreshold -f %PDFFILE% -c quit
